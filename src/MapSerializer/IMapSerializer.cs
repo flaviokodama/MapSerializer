dbbf56fa594d14a0d2342d26d6950fc295228d75
flaviokodama <flavio_kodama@hotmail.com>
2021-08-26 18:03:46 -0700

    Added some Summary to interfaces and public classes

--- src/MapSerializer/IMapSerializer.cs
+++ src/MapSerializer/IMapSerializer.cs
@@ -2,9 +2,23 @@
 
 namespace MapSerializer
 {
+    /// <summary>
+    /// Represents a serializer that works by writing mapped types and their properties.
+    /// </summary>
     public interface IMapSerializer
     {
+        /// <summary>
+        /// Creates a map to type of which objects will be serialized.
+        /// </summary>
+        /// <typeparam name="T">Type being mapped.</typeparam>
+        /// <returns><see cref="ITypeMap{T}"/> object that allows properties be mapped.</returns>
         ITypeMap<T> MapType<T>();
+
+        /// <summary>
+        /// Serializes an object.
+        /// </summary>
+        /// <param name="writer">Destination text writer.</param>
+        /// <param name="reference">Object reference.</param>
         void Serialize(TextWriter writer, object reference);
     }
 }
--- src/MapSerializer/ITypeMap.cs
+++ src/MapSerializer/ITypeMap.cs
@@ -3,8 +3,18 @@ using System.Linq.Expressions;
 
 namespace MapSerializer
 {
+    /// <summary>
+    /// Represents a map to the type of which objects will be serialized.
+    /// </summary>
+    /// <typeparam name="T">Type marked to serialization.</typeparam>
     public interface ITypeMap<T>
     {
+        /// <summary>
+        /// Maps a property to be serialized.
+        /// </summary>
+        /// <typeparam name="TProp">Property type.</typeparam>
+        /// <param name="propertyExpression">Expression used to select a property to be serialized.</param>
+        /// <returns>Returns an <see cref="IPropertyMap{T, TProp}"/> object that allows more properties to be mapped easily.</returns>
         IPropertyMap<T, TProp> MapProperty<TProp>(Expression<Func<T, TProp>> propertyExpression);
     }
 }
