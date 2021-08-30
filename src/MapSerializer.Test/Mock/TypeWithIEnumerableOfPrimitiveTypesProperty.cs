0c7e3ab3ad1c3cb1635d6751fe1beef614a42a5f
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2021-08-30 18:19:00 -0300

    test: mock to test serialization of int Ienumerable properties

---
+++ src/MapSerializer.Test/Mock/TypeWithIEnumerableOfPrimitiveTypesProperty.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MapSerializer.Test.Mock
+{
+    public class TypeWithIEnumerableOfPrimitiveTypesProperty
+    {
+        public List<int> ListOfIntegers { get; set; }
+    }
+}
